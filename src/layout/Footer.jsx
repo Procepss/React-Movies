e4178e1dc7cc8dda99ddc76262bfdfc4a7e34f03
Procepss <arti_rnd@mail.ru>
2022-05-26 13:53:44 +0300

    new style

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,16 @@
 function Footer() {
-    return <footer className="page-footer green lighten-4">
-    <div className="footer-copyright">
-      <div className="container">
-      © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+  return (
+    <footer className="page-footer light-blue lighten-2">
+      <div className="footer-copyright">
+        <div className="container">
+          © {new Date().getFullYear()} Copyright Text
+          <a className="grey-text text-lighten-4 right" href="#!">
+            Repo
+          </a>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  );
 }
 
-export {Footer}
+export { Footer };
